dc38c62205f9c8d65398b7ecd0836704f854a8b0
David H. Bronke <dbronke@armt.com>
2014-09-16 23:51:09 -0500

    Updated bulleterl.cpp to work with BulletEngine.

--- c_src/bulleterl.cpp
+++ c_src/bulleterl.cpp
@@ -1,60 +1,54 @@
 #include "erl_nif.h"
 
+#include "BulletEngine.h"
 
-static ErlNifResourceType* bulleterl_RESOURCE = NULL;
 
-struct bulleterl_handle
+static ErlNifResourceType* bulleterlType = NULL;
+
+
+namespace bulleterl
 {
-}; // end bulleterl_handle
+
+	static ERL_NIF_TERM new_(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+	{
+		BulletEngine* engine = new (enif_alloc_resource(bulleterlType, sizeof(BulletEngine))) BulletEngine();
+		ERL_NIF_TERM result = enif_make_resource(env, engine);
+		enif_release_resource(engine);
+
+		return enif_make_tuple2(env, enif_make_atom(env, "bulleterl"), result);
+	} // end new_
 
 
-// Prototypes
-static ERL_NIF_TERM bulleterl_new(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
-static ERL_NIF_TERM bulleterl_myfunction(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+	static ERL_NIF_TERM myfunction(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+	{
+		return enif_make_atom(env, "ok");
+	} // end myfunction
+
+	static void cleanup(ErlNifEnv* env, void* obj)
+	{
+		BulletEngine* handle = (BulletEngine*) obj;
+		handle->~BulletEngine();
+	} // end cleanup
+
+	static int on_load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
+	{
+		ErlNifResourceFlags flags = ErlNifResourceFlags(ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER);
+
+		ErlNifResourceType* rt;
+
+		rt = enif_open_resource_type(env, NULL, "bulleterl", &bulleterl::cleanup, flags, NULL);
+		if(rt == NULL) { return -1; }
+		bulleterlType = rt;
+
+		return 0;
+	} // end on_load
+
+}; // end bulleterl
 
 
 static ErlNifFunc nif_funcs[] = {
-    {"new", 0, bulleterl_new},
-    {"myfunction", 1, bulleterl_myfunction}
+    {"new", 0, bulleterl::new_},
+    {"myfunction", 1, bulleterl::myfunction}
 }; // end nif_funcs
 
-
-static ERL_NIF_TERM bulleterl_new(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
-{
-    bulleterl_handle* handle = (bulleterl_handle*) enif_alloc_resource(bulleterl_RESOURCE, sizeof(bulleterl_handle));
-    ERL_NIF_TERM result = enif_make_resource(env, handle);
-    enif_release_resource(handle);
-
-    return enif_make_tuple2(env, enif_make_atom(env, "ok"), result);
-} // end bulleterl_new
-
-
-static ERL_NIF_TERM bulleterl_myfunction(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
-{
-    return enif_make_atom(env, "ok");
-} // end bulleterl_myfunction
-
-static void bulleterl_resource_cleanup(ErlNifEnv* env, void* arg)
-{
-    /* Delete any dynamically allocated memory stored in bulleterl_handle */
-    /* bulleterl_handle* handle = (bulleterl_handle*)arg; */
-} // end bulleterl_resource_cleanup
-
-static int on_load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
-{
-    ErlNifResourceFlags flags = ErlNifResourceFlags(ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER);
-
-    ErlNifResourceType* rt = enif_open_resource_type(env, NULL,
-			"bulleterl_resource", &bulleterl_resource_cleanup, flags, NULL);
-
-    if(rt == NULL)
-	{
-        return -1;
-	} // end if
-
-    bulleterl_RESOURCE = rt;
-
-    return 0;
-} // end on_load
-
-ERL_NIF_INIT(bulleterl, nif_funcs, &on_load, NULL, NULL, NULL);
+ERL_NIF_INIT(bulleterl, nif_funcs, &bulleterl::on_load, NULL, NULL, NULL);
